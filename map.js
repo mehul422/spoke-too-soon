f8f73db451f47006c3f998d2df038829d6f7e5ba
Mehul Verma <meverma@ucsd.edu>
2025-02-21 01:25:32 -0800

    fixed cambridge bike lanes

--- map.js
+++ map.js
@@ -1,46 +1,46 @@
-  // Set your Mapbox access token here
-  mapboxgl.accessToken = 'pk.eyJ1IjoibWV2ZXJtYSIsImEiOiJjbTdlZ3N2c3YwZXNvMnNxM3E0aGhmdTNpIn0.D9hqfcrunVuizVHn-NBhNg';
+// Set your Mapbox access token
+mapboxgl.accessToken = 'pk.eyJ1IjoibWV2ZXJtYSIsImEiOiJjbTdlZ3N2c3YwZXNvMnNxM3E0aGhmdTNpIn0.D9hqfcrunVuizVHn-NBhNg';
 
-  // Initialize the map
-  const map = new mapboxgl.Map({
+// Initialize the map
+const map = new mapboxgl.Map({
     container: 'map', // ID of the div where the map will render
     style: 'mapbox://styles/mapbox/satellite-streets-v12', // Map style
     center: [-71.09415, 42.36027], // [longitude, latitude]
     zoom: 12, // Initial zoom level
     minZoom: 5, // Minimum allowed zoom
     maxZoom: 18 // Maximum allowed zoom
-  });
+});
 
-  map.on('load', () => { 
+map.on('load', () => { 
+    // Add Boston bike lanes
     map.addSource('boston_data_route', {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
-      });
+    });
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'boston-bike-lanes', // Unique ID for Boston's bike lanes
         type: 'line',
         source: 'boston_data_route',
         paint: {
-          'line-color': '#32D400',
-          'line-width': 5,
-          'line-opacity': 0.6
+            'line-color': '#32D400',
+            'line-width': 5,
+            'line-opacity': 0.6
         }
-      });  
-  });
+    });
 
-  map.on('load', () => { 
+    // Add Cambridge bike lanes
     map.addSource('cambridge_data_route', {
         type: 'geojson',
         data: 'https://data.cambridgema.gov/api/geospatial/9aey-9g9p?method=export&format=GeoJSON'
-      });
+    });
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'cambridge-bike-lanes', // Unique ID for Cambridge's bike lanes
         type: 'line',
         source: 'cambridge_data_route',
         paint: {
-          'line-color': '#32D400',
-          'line-width': 5,
-          'line-opacity': 0.6
+            'line-color': '#FF5733', // Different color to distinguish
+            'line-width': 5,
+            'line-opacity': 0.6
         }
-      });        
-  });
+    });        
+});
